fb710c89a64efcbeeca063e04c921d0ea22fb894
Michael Mulchrone <mtmulch@gmail.com>
2017-09-20 21:26:53 -0500

    Moving master cluster process into a seperate file from the the express worker processes.

--- server/cluster/server.ts
+++ server/cluster/server.ts
@@ -1,14 +1,12 @@
-import cluster = require("cluster");
 import debug = require("debug");
 import express = require("express");
 import path = require("path");
 import logger = require("morgan");
 import bodyParser = require("body-parser");
 
-import { IndexRouter } from "./routes/index-router";
-import { Database } from "./globals/Database";
+import { IndexRouter } from "../routes/index-router";
 
-class Server {
+export default class Server {
   public port = process.env.PORT || 8080;
   public app: express.Application;
   public indexRouter: express.Router;
@@ -29,27 +27,4 @@ class Server {
     this.app.disable("x-powered-by");
     this.app.use("/", this.indexRouter);
   }
-}
-
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-  console.log(`Master ${process.pid} running database seed`);
-  if (Database.SeedDatabase()) {
-    console.log(`Master ${process.pid} database seed complete`);
-  }
-
-  const numCPUs = require("os").cpus().length;
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    cluster.fork();
-  });
-} else {
-  const newServer = new Server();
-  const serverWorker = newServer.app.listen(newServer.port, () => {
-    console.log(`Server is listening on ${newServer.port}`);
-  });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    server: './server/server.ts'
+    server: './server/cluster/master.ts'
   },
   resolve: {
     extensions: ['.ts', '.js'],
