01c1a3ffb882ee50cfa50029531c034a43999b65
Michael Mulchrone <mtmulch@gmail.com>
2017-10-07 11:11:02 -0500

    Just another check to make sure that this client is authorized to be at this dashboard.

--- client/app/modules/users/dashboard/dashboard.component.ts
+++ client/app/modules/users/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { ActivatedRoute } from "@angular/router";
+import { JsonWebToken } from "../../../shared/models/user-authenication.model";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
 import { DashboardService } from "../../../shared/services/dashboard.service";
 import { Dashboard } from "../../../shared/models/dashboard.model";
+import { AuthenicationControl } from "../../../shared/authenication/AuthenicationControl";
 
 @Component({
   selector: "app-users-dashboard",
@@ -14,10 +16,22 @@ export class DashboardComponent implements OnInit {
 
   constructor(
     private dashboardService: DashboardService,
-    private route: ActivatedRoute
+    private authControl: AuthenicationControl,
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
+    this.isUserAuthorizedToBeHere();
+  }
 
+  private isUserAuthorizedToBeHere() {
+    this.route.params.subscribe(params => {
+      const userIdParam: string = params["id"];
+      const token: JsonWebToken = this.authControl.getDecodedToken();
+      if (token.id !== userIdParam) {
+        this.router.navigate(["../../users/login"]);
+      }
+    });
   }
 }
