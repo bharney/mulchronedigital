8af9f8046b72dc1e6f8e52dd17a6aa6b2f70e337
Michael Mulchrone <mtmulch@gmail.com>
2017-10-07 10:24:50 -0500

    Changing function named, token is stored in local storage now.

--- client/app/shared/authenication/AuthenicationControl.ts
+++ client/app/shared/authenication/AuthenicationControl.ts
@@ -5,36 +5,47 @@ import * as jwtDecode from "jwt-decode";
 @Injectable()
 export class AuthenicationControl {
 
-    public storeJsonWebToken(jsonWebToken: string): void {
-        localStorage.setItem("authenication-token", jsonWebToken);
-    }
+  public storeJsonWebToken(jsonWebToken: string): void {
+    localStorage.setItem("authenication-token", jsonWebToken);
+  }
 
-    public getJsonWebTokenFromSessionStorage(): string {
-        return localStorage.getItem("authenication-token");
-    }
+  public getJsonWebTokenFromLocalStorage(): string {
+    return localStorage.getItem("authenication-token");
+  }
 
-    public removeJsonWebToken(): void {
-        localStorage.removeItem("authenication-token");
-    }
+  public removeJsonWebToken(): void {
+    localStorage.removeItem("authenication-token");
+  }
 
-    public isTheUserAuthenicated(): boolean {
-        const token = this.getJsonWebTokenFromSessionStorage();
-        if (token === null) {
-            return false;
-        } else {
-            return true;
-        }
-      }
-
-    public createAuthorizationHeader(): RequestOptions {
-        // if the user doesnt not have a token stored stop
-        if (!this.isTheUserAuthenicated) {
-            return;
-        } else {
-            const options = new RequestOptions({ headers: new Headers() });
-            const token = this.getJsonWebTokenFromSessionStorage();
-            options.headers.set("x-access-token", token);
-            return options;
-        }
+  public isTheUserAuthenicated(): boolean {
+    const token = this.getJsonWebTokenFromLocalStorage();
+    if (token === null) {
+      return false;
+    } else {
+      return true;
     }
+  }
+
+  public createAuthorizationHeader(): RequestOptions {
+    // if the user doesnt not have a token stored stop
+    if (!this.isTheUserAuthenicated) {
+      return;
+    } else {
+      const options = new RequestOptions({ headers: new Headers() });
+      const token = this.getJsonWebTokenFromLocalStorage();
+      options.headers.set("x-access-token", token);
+      return options;
+    }
+  }
+
+  public getDecodedToken(token: string) {
+    const decodedToken = jwtDecode(token);
+    return decodedToken;
+  }
+
+  public createRequestOptionsWithApplicationJsonHeaders(): RequestOptions {
+    const headers = new Headers({ "Content-Type": "application/json" });
+    const requestOptions = new RequestOptions({ headers: headers });
+    return requestOptions;
+  }
 }
