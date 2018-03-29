26a4b8b258282e92eb17da4e8cfaee6179a2698a
Mike Mulchrone <mtmulch@gmail.com>
2018-03-28 22:01:54 -0500

    #21 testing valid admin token with getusers route.

--- test/api/BaseRouter.test.ts
+++ test/api/BaseRouter.test.ts
@@ -5,20 +5,40 @@ const assert = chai.assert;
 chai.use(chaiHttp);
 import { createLoginUserObject } from "../helpers/LoginHelpers";
 
+const host = "http://localhost:8080";
+const loginUserPath = "/api/userauth/loginuser";
+
 const getRegularUserToken = () => {
     return new Promise(async (resolve, reject) => {
         const host = "http://localhost:8080";
-        const path = "/api/userauth/loginuser";
+        const loginUserPath = "/api/userauth/loginuser";
         const userPassword = "Password1234!@#$";
         const userEmail = "basicuser@gmail.com";
         const encryptedLoginInfo = await createLoginUserObject(userPassword, userEmail);
         chai.request(host)
-            .post(path)
+            .post(loginUserPath)
             .set("Content-Type", "application/json")
             .send(encryptedLoginInfo)
             .then(response => {
-                const token = response.body.token;
-                resolve(token);
+                resolve(response.body.token);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+const getAdminUserToken = () => {
+    return new Promise(async (resolve, reject) => {
+        const userPassword = "Password1234!@#$";
+        const userEmail = "admin@gmail.com";
+        const encryptedLoginInfo = await createLoginUserObject(userPassword, userEmail);
+        chai.request(host)
+            .post(loginUserPath)
+            .set("Content-Type", "application/json")
+            .send(encryptedLoginInfo)
+            .then(response => {
+                resolve(response.body.token);
             })
             .catch(error => {
                 reject(error);
@@ -27,13 +47,13 @@ const getRegularUserToken = () => {
 };
 
 describe("BaseRouter Tests", () => {
-    // I suppose it really doesnt matter the exact route we test, I'm just testing the methods the other routers in inherit 
-    // from the abstract class BaseRouter.
-    const host = "http://localhost:8080";
+    // I suppose it really doesnt matter the exact route we test, I'm just testing the methods the other routers in inherit from the abstract class BaseRouter.
     const path = "/api/admindashboard/getusers";
-    let tokenToTest;
+    let regularUserToken;
+    let adminUserToken;
     before(async () => {
-        tokenToTest = await getRegularUserToken();
+        regularUserToken = await getRegularUserToken();
+        adminUserToken = await getAdminUserToken();
     });
 
     it("it should say no token in header", async () => {
@@ -48,16 +68,11 @@ describe("BaseRouter Tests", () => {
             });
     });
 
-    
-
-
     it("it should say the user account is not an admin", async () => {
-        // const userToken = await UserCollection
         return chai.request(host)
             .get(path)
-            .set("mulchronedigital-token", tokenToTest)
-            .catch(async error => {
-                console.log(error.response.text);
+            .set("mulchronedigital-token", regularUserToken)
+            .catch(error => {
                 assert.equal(error.status, 503);
                 const body = JSON.parse(error.response.text);
                 assert.equal(body.status, false);
@@ -65,5 +80,22 @@ describe("BaseRouter Tests", () => {
                 const messageToTest = "The user account is not an admin, if you previously had admin access your access maybe have been revoked";
                 assert.equal(body.message, messageToTest);
             });
+    });
+
+    it("it should return a response body of true and some users in an array", async () => {
+        return chai.request(host)
+        .get(path)
+        .set("mulchronedigital-token", regularUserToken)
+        .then(response => {
+            assert.equal(response.status, 200);
+            const body = JSON.parse(response.body.text);
+            assert.equal(body.status, true);
+            assert.isArray(body.users);
+            const lengthOfUsers = body.users.length;
+            assert.equal((lengthOfUsers > 0), true);
+        })
+        .catch(error => {
+
+        });
     });
 });
