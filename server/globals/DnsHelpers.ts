b5508d5d4dc5a565f41280974bccd590eac3b0eb
Mike Mulchrone <mtmulch@gmail.com>
2018-03-28 23:43:02 -0500

    #81 reverse DNS lookup function and test cases.

--- server/globals/DnsHelpers.ts
+++ server/globals/DnsHelpers.ts
@@ -1,5 +1,17 @@
 import dns = require("dns");
 
 export class DnsHelpers {
-
+    public static reverseDNSLookup(ipAddress: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            if (!ipAddress){ 
+                reject(null);
+            }
+            dns.reverse(ipAddress, (err, hostnames) => {
+                if (err) {
+                    reject(err);
+                }
+                (!hostnames) ? resolve(null) : resolve(hostnames[0]);
+            });
+        });
+    }
 }
