d82a008d0050ec9c8d9a2e35a0ebebe167846d5b
Mike Mulchrone <mtmulch@gmail.com>
2018-03-30 10:51:32 -0500

    #86 configuring test module for admin-dashboard-navigation component

--- client/app/modules/dashboard/admin-dashboard/components/admin-dashboard-navigation/admin-dashboard-navigation.component.spec.ts
+++ client/app/modules/dashboard/admin-dashboard/components/admin-dashboard-navigation/admin-dashboard-navigation.component.spec.ts
@@ -4,6 +4,9 @@ import { By } from "@angular/platform-browser";
 import { DebugElement } from "@angular/core";
 
 import { AdminDashboardNavigationComponent } from "./admin-dashboard-navigation.component";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AuthenicationControl } from "../../../../../shared/authenication/AuthenicationControl";
+import { AdminDashboardEmitter } from "../../../../../shared/services/emitters/admin-dashboard-emitter.service";
 
 describe("AdminDashboardNavigationComponent", () => {
   let component: AdminDashboardNavigationComponent;
@@ -11,9 +14,17 @@ describe("AdminDashboardNavigationComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminDashboardNavigationComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [ 
+        AdminDashboardNavigationComponent 
+      ],
+      providers: [
+        AuthenicationControl,
+        AdminDashboardEmitter
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
