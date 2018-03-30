c8c0543c07e8cef50ddff30465ab506c358f4182
Mike Mulchrone <mtmulch@gmail.com>
2018-03-30 12:57:14 -0500

    #21 refresh token route test case.

--- test/helpers/DatabaseHelpers.ts
+++ test/helpers/DatabaseHelpers.ts
@@ -1,11 +1,13 @@
-import { UsersCollection, UserActionsCollection } from "../../server/config/master";
+import { UsersCollection, UserActionsCollection, usersCollectionIsFalsy } from "../../server/config/master";
 
 export class DatabaseHelpers {
     public static async deleteUserFromDatabaseByUsername(username: string): Promise<boolean> {
         try {
             if (!username)
                 return false;
-
+            if (!UsersCollection) {
+                await usersCollectionIsFalsy();
+            }
             const deleteResult = await UsersCollection.deleteOne({ username: username });
             if (deleteResult.result.n === 1 && deleteResult.result.ok === 1)
                 return true;
@@ -13,6 +15,20 @@ export class DatabaseHelpers {
             return false;
         } catch (error) {
             return false;
+        }
+    }
+
+    public static async getUsersJsonWebTokenByUsername(username: string): Promise<string> {
+        try {
+            if (!username)
+                return null;
+            if (!UsersCollection) {
+                await usersCollectionIsFalsy();
+            }
+            const result = await UsersCollection.findOne({ username: username }, { _id: 0, jsonToken: 1 });
+            return result.jsonToken;
+        } catch (error) {
+            return null;
         }
     }
 }
