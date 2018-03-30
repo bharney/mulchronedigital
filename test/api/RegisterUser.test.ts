2f0a702b694d33c106787374c6bfc10f3a8be8fc
Mike Mulchrone <mtmulch@gmail.com>
2018-03-30 00:48:58 -0500

    #21 email is taken test and successfully user registration.
    Delete that user when the test is done running.

--- test/api/RegisterUser.test.ts
+++ test/api/RegisterUser.test.ts
@@ -3,6 +3,7 @@ import * as chai from "chai";
 import chaiHttp = require("chai-http");
 import LoginHelpers from "../helpers/LoginHelpers";
 import { AESEncryptionResult } from "../../shared/Encryption";
+import { DatabaseHelpers } from "../helpers/DatabaseHelpers";
 const assert = chai.assert;
 chai.use(chaiHttp);
 
@@ -75,6 +76,45 @@ describe("Register User Route Path Tests", () => {
                 const body = JSON.parse(error.response.text);
                 assert.equal(body.status, false);
                 assert.equal(body.message, `The username ${username} is already taken`);
+            });
+    });
+
+    it("it should say the email is already taken", async () => {
+        const username = "admin234";
+        const email = "admin@gmail.com";
+        const password = "Password1234!@#$";
+        const encryptedRegisterUserObject: AESEncryptionResult = await LoginHelpers.createRegisterUserObject(username, email, password);
+        return chai.request(host)
+            .post(path)
+            .set("Content-Type", "application/json")
+            .send(encryptedRegisterUserObject)
+            .catch(error => {
+                assert.equal(error.status, 409);
+                const body = JSON.parse(error.response.text);
+                assert.equal(body.status, false);
+                assert.equal(body.message, `The email ${email} is already associated with an account.`);
+            });
+    });
+
+    it("it should say user registration was successful", async () => {
+        const username = "testuser";
+        const email = "testuser@gmail.com";
+        const password = "Password1234!@#$";
+        const encryptedRegisterUserObject: AESEncryptionResult = await LoginHelpers.createRegisterUserObject(username, email, password);
+        return chai.request(host)
+            .post(path)
+            .set("Content-Type", "application/json")
+            .send(encryptedRegisterUserObject)
+            .then(async response => {
+                assert.equal(response.status, 200);
+                const body = response.body;
+                assert.equal(body.status, true);
+                const messageToTest = `Thanks for registering ${username}!` 
+                assert.equal(body.message.includes(messageToTest), true);
+                const messageToTestTwo = `We have sent a email to ${email}`;
+                assert.equal(body.message.includes(messageToTestTwo), true);
+                const result = await DatabaseHelpers.deleteUserFromDatabaseByUsername(username);
+                assert.equal(result, true);
             });
     });
 });
--- test/helpers/DatabaseHelpers.ts
+++ test/helpers/DatabaseHelpers.ts
@@ -1,3 +1,18 @@
+import { UsersCollection, UserActionsCollection } from "../../server/config/master";
+
 export class DatabaseHelpers {
-    
+    public static async deleteUserFromDatabaseByUsername(username: string): Promise<boolean> {
+        try {
+            if (!username)
+                return false;
+
+            const deleteResult = await UsersCollection.deleteOne({ username: username });
+            if (deleteResult.result.n === 1 && deleteResult.result.ok === 1)
+                return true;
+
+            return false;
+        } catch (error) {
+            return false;
+        }
+    }
 }
