82a5e651c754ee07dedfdd0e2f167b1e448d5b32
Mike Mulchrone <mtmulch@gmail.com>
2018-03-30 11:11:02 -0500

    #86 adjusting test module for UserDashboardNavigationComponent

--- client/app/modules/dashboard/user-dashboard/components/user-dashboard-navigation/user-dashboard-navigation.component.spec.ts
+++ client/app/modules/dashboard/user-dashboard/components/user-dashboard-navigation/user-dashboard-navigation.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed, inject } from "@angular/core/testing";
 
 import { UserDashboardNavigationComponent } from "./user-dashboard-navigation.component";
+import { UserDashboardEmitter } from "../../../../../shared/services/emitters/user-dashboard-emitter.service";
 
-describe("a dashboard-navigation component", () => {
+describe("UserDashboardNavigationComponent", () => {
 	let component: UserDashboardNavigationComponent;
 
 	// register all needed dependencies
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			providers: [
-				UserDashboardNavigationComponent
+				UserDashboardNavigationComponent,
+				UserDashboardEmitter
 			]
 		});
 	});
