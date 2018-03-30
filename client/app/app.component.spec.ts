068ee361ed73d3f9d2f49806ea18b9ed655cb9bb
Mike Mulchrone <mtmulch@gmail.com>
2018-03-30 10:35:17 -0500

    #86 getting rid of errors in app component tests.

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -1,13 +1,31 @@
 import { TestBed, async } from "@angular/core/testing";
 
 import { AppComponent } from "./app.component";
+import { NavbarTopComponent } from "./components/navbar-top/navbar-top.component";
+import { AppRoutingModule } from "./app.routing.module";
+import { APP_BASE_HREF } from "@angular/common";
+import { AuthenicationControl } from "./shared/authenication/AuthenicationControl";
+import { ApiRequests } from "./shared/http/ApiRequests";
+import { RefreshTokenService } from "./shared/services/refresh-token.service";
+import { GoogleAnalytics } from "./shared/services/google-analytics.service";
+import { HttpModule } from "@angular/http";
+import { BrowserModule } from "@angular/platform-browser";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarTopComponent
       ],
+      imports: [BrowserModule, HttpModule, AppRoutingModule],
+      providers: [
+        AuthenicationControl,
+        ApiRequests,
+        RefreshTokenService,
+        GoogleAnalytics,
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
     }).compileComponents();
   }));
 
