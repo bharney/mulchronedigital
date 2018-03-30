45f61418eeb18493ab61e246231aca6df289eced
Mike Mulchrone <mtmulch@gmail.com>
2018-03-30 11:08:24 -0500

    #86 adjusting test module for UserDashboardComponent

--- client/app/modules/dashboard/user-dashboard/user-dashboard.component.spec.ts
+++ client/app/modules/dashboard/user-dashboard/user-dashboard.component.spec.ts
@@ -1,15 +1,32 @@
 import { TestBed, inject } from "@angular/core/testing";
 
 import { UserDashboardComponent } from "./user-dashboard.component";
+import { MainDashboardService } from "../../../shared/services/user-dashboard.service";
+import { HttpModule } from "@angular/http";
+import { ApiRequests } from "../../../shared/http/ApiRequests";
+import { AuthenicationControl } from "../../../shared/authenication/AuthenicationControl";
+import { ActivatedRoute } from "@angular/router";
+import { GoogleAnalytics } from "../../../shared/services/google-analytics.service";
+import { RouterTestingModule } from "@angular/router/testing";
+import { UserDashboardEmitter } from "../../../shared/services/emitters/user-dashboard-emitter.service";
 
-describe("a user-dashboard component", () => {
+describe("UserDashboardComponent", () => {
   let component: UserDashboardComponent;
 
   // register all needed dependencies
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpModule,
+        RouterTestingModule
+      ],
       providers: [
-        UserDashboardComponent
+        UserDashboardComponent,
+        MainDashboardService,
+        ApiRequests,
+        AuthenicationControl,
+        GoogleAnalytics,
+        UserDashboardEmitter
       ]
     });
   });
