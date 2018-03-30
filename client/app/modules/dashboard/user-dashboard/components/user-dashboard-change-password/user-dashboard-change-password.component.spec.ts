8e5a66998e08f5f62748e3f42a9d7ab04476a2bb
Mike Mulchrone <mtmulch@gmail.com>
2018-03-30 11:00:23 -0500

    #86 adjusting test module for user-dashboard-change-password component.

--- client/app/modules/dashboard/user-dashboard/components/user-dashboard-change-password/user-dashboard-change-password.component.spec.ts
+++ client/app/modules/dashboard/user-dashboard/components/user-dashboard-change-password/user-dashboard-change-password.component.spec.ts
@@ -1,15 +1,32 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { UserDashboardChangePasswordComponent } from './user-dashboard-change-password.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChangeUserPasswordService } from '../../../../../shared/services/user-dashboard.service';
+import { HttpModule } from '@angular/http';
+import { ApiRequests } from '../../../../../shared/http/ApiRequests';
+import { GoogleAnalytics } from '../../../../../shared/services/google-analytics.service';
+import { AuthenicationControl } from '../../../../../shared/authenication/AuthenicationControl';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('a user-dashboard-change-password component', () => {
+describe('UserDashboardChangePasswordComponent', () => {
 	let component: UserDashboardChangePasswordComponent;
 
 	// register all needed dependencies
 	beforeEach(() => {
 		TestBed.configureTestingModule({
+			imports: [
+				HttpModule,
+				FormsModule,
+				ReactiveFormsModule, 
+				RouterTestingModule
+			],
 			providers: [
-				UserDashboardChangePasswordComponent
+				UserDashboardChangePasswordComponent,
+				ChangeUserPasswordService,
+				ApiRequests,
+				GoogleAnalytics,
+				AuthenicationControl
 			]
 		});
 	});
