d94aa0068119a90c339aee1c83f1f387a5e590b6
Mike Mulchrone <mtmulch@gmail.com>
2018-04-07 12:27:38 -0500

    #21 findUserLoginDetailsByEmail test cases.

--- test/unit/DataAccess.test.ts
+++ test/unit/DataAccess.test.ts
@@ -20,6 +20,33 @@ describe("DataAccess Base Class Test", () => {
         assert.equal(DataAccess.hasOwnProperty("findUserLoginDetailsByEmail"), true);
     }); 
 
+    it("it should return an empty array because there is not a user with this email", async () => {
+        const badUserEmail = "welcome@gmail.com";
+        const databaseUsers: User[] = await DataAccess.findUserLoginDetailsByEmail(badUserEmail);
+        assert.equal(typeof(databaseUsers), typeof([]));
+        assert.equal(databaseUsers.length, 0);
+    });
+
+    it("it should return the login details for the admin user", async () => {
+        const email = "admin@gmail.com";
+        const databaseUsers: User[] = await DataAccess.findUserLoginDetailsByEmail(email);
+        const user = databaseUsers[0];
+        assert.equal(user.hasOwnProperty("_id"), true);
+        assert.equal(user.hasOwnProperty("password"), true);
+        assert.equal(user.hasOwnProperty("username"), true);
+        assert.equal(user.hasOwnProperty("isAdmin"), true);
+        assert.equal(user.hasOwnProperty("isActive"), true);
+        assert.equal(user.hasOwnProperty("publicKeyPairOne"), true);
+        assert.equal(user.hasOwnProperty("privateKeyPairTwo"), true);
+    });
+
+    it("it should return any empty array because an invalid parameter was passed to findUserLoginDetailsByEmail(userEmail: string)", async () => {
+        const email = null;
+        const result = await DataAccess.findUserLoginDetailsByEmail(email);
+        assert.equal(result.length, 0);
+        assert.equal(typeof(result), typeof([]));
+    });
+
     it("it should have a method of getUserPassword(userId: string)", () => {
         assert.equal(DataAccess.hasOwnProperty("getUserPassword"), true);
     });
@@ -36,7 +63,7 @@ describe("DataAccess Base Class Test", () => {
         });
     });
 
-    it("it should return any empty array because an invalid parameter was passed", async () => {
+    it("it should return any empty array because an invalid parameter was passed to getUserPassword(userId: string)", async () => {
         const badUserId = null;
         const userName = await DataAccess.getUserPassword(badUserId);
         assert.equal(typeof(userName), typeof([]));
