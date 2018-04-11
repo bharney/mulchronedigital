d6fa8dc51ad7a1555caa4d2a47e1c5d47eb2fd72
Mike Mulchrone <mtmulch@gmail.com>
2018-04-11 18:42:53 -0500

    #96 moving register user into its own router.

--- server/routes/users/UserAuthenicationIndexRouter.ts
+++ server/routes/users/UserAuthenicationIndexRouter.ts
@@ -17,10 +17,16 @@ import { DataAccess } from "../../data-access/classes/DataAccess";
 import { ServerEncryption } from "../../security/ServerEncryption";
 import { DnsHelpers } from "../../globals/DnsHelpers";
 import LoginUserRouter from "./LoginUserRouter";
+import RegisterUserRouter from "./RegisterUserRouter";
 
 export default class UserAuthenicationIndexRouter extends BaseRouter {
   public router: Router;
   private loginUserRouter: Router;
+  private registerUserRouter: Router;
+  private refreshTokenRouter: Router;
+  private activateUserRouter: Router;
+  private forgotPasswordRouter: Router;
+  private resetPasswordRouter: Router;
 
   constructor() {
     super();
@@ -31,17 +37,12 @@ export default class UserAuthenicationIndexRouter extends BaseRouter {
 
   private createSubRouters(): void {
       this.loginUserRouter = new LoginUserRouter().router;
+      this.registerUserRouter = new RegisterUserRouter().router;
   }
 
   private configureRouter(): void {
     this.router.use("/loginuser", this.loginUserRouter);
-
-
-    // Register user
-    this.router.use("/registeruser", this.decryptRequestBody);
-    this.router.use("/registeruser", this.validateRegisterUserRequest);
-    this.router.use("/registeruser", this.doesUsernameOrEmailExistAlready);
-    this.router.post("/registeruser", this.insertNewUser);
+    this.router.use("/registeruser", this.registerUserRouter);
 
     // RefreshJsonWebToken
     this.router.use("/refreshtoken", this.checkForUserJsonWebToken);
@@ -56,67 +57,6 @@ export default class UserAuthenicationIndexRouter extends BaseRouter {
     // Reset password
     this.router.use("/resetpassword", this.decryptRequestBody);
     this.router.use("/resetpassword", this.validateResetPassword);
-  }
-
-  private async validateRegisterUserRequest(req: Request, res: Response, next: NextFunction) {
-    try {
-      if (!await UserAuthenicationValidator.isUserNameValid(req.body.username)) {
-        return res.status(422).json(await ResponseMessages.userNameIsNotValid());
-      }
-      if (!await UserAuthenicationValidator.isEmailValid(req.body.email)) {
-        return res.status(422).json(await ResponseMessages.emailIsNotValid());
-      }
-      if (!await UserAuthenicationValidator.isPasswordValid(req.body.password)) {
-        return res.status(422).json(await ResponseMessages.passwordIsNotValid());
-      }
-      next();
-    } catch (error) {
-      res.status(503).json(await ResponseMessages.generalError());
-      return next(error);
-    }
-  }
-
-  private async doesUsernameOrEmailExistAlready(req: Request, res: Response, next: NextFunction) {
-    try {
-      const databaseUser: User[] = await UserAuthenicationDataAccess.findIfUserExistsByUsername(req.body.username);
-      if (databaseUser.length > 0) {
-        return res.status(409).json(await ResponseMessages.usernameIsTaken(req.body.username));
-      }
-      const databaseEmail: User[] = await UserAuthenicationDataAccess.findIfUserExistsByEmail(req.body.email);
-      if (databaseEmail.length > 0) {
-        return res.status(409).json(await ResponseMessages.emailIsTaken(req.body.email));
-      }
-      next();
-    } catch (error) {
-      res.status(503).json(await ResponseMessages.generalError());
-      return next(error);
-    }
-  }
-
-  private async insertNewUser(req: Request, res: Response, next: NextFunction) {
-    try {
-      const httpHelpers = new HttpHelpers();
-      const ip = await httpHelpers.getIpAddressFromRequestObject(req.ip);
-      const domain = await DnsHelpers.reverseDNSLookup(ip);
-      const userAgent = await httpHelpers.getUserAgentFromRequestObject(req.headers);
-      const ipAddressObject = new UserIpAddress(ip, userAgent, domain);
-      const newUser = new User(req.body.username, req.body.email, req.body.password, ipAddressObject);
-      // TODO: split this up into seperate functions. little messy;
-      if (await newUser.SetupNewUser()) {
-        const insertResult = await UserAuthenicationDataAccess.insertNewUser(newUser);
-        if (insertResult.result.n === 1) {
-          res.status(200).json(await ResponseMessages.userRegistrationSuccessful(req.body.username, req.body.email));
-          EmailQueueExport.sendUserActivationEmailToQueue(newUser);
-        } else {
-          return res.status(503).json(await ResponseMessages.generalError());
-        }
-      } else {
-        return res.status(503).json(await ResponseMessages.generalError());
-      }
-    } catch (error) {
-      res.status(503).json(await ResponseMessages.generalError());
-      return next(error);
-    }
   }
 
   private async validateRefreshJsonWebToken(req: Request, res: Response, next: NextFunction) {
