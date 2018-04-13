35341464dcd0830abe2c601c9fc8558a142e1509
Mike Mulchrone <mtmulch@gmail.com>
2018-04-13 01:10:26 -0500

    #73 refactoring LoginService

--- client/app/shared/services/user-authenication.service.ts
+++ client/app/shared/services/user-authenication.service.ts
@@ -6,10 +6,11 @@ import { LoginUser, RegisterUser, IUserRegisterResponse, ILoginUserResponse } fr
 import { ApiRequests } from "../http/ApiRequests";
 import { AuthenicationControl } from "../authenication/AuthenicationControl";
 import { AESEncryptionResult } from "../../../../shared/AESEncryptionResult";
+import IService from "./interfaces/IService";
 
 @Injectable()
-export class LoginService {
-
+export class LoginService implements IService {
+  public codesToNotRetry: number[] = [401, 422];
   constructor(
     private http: Http,
     private apiRequests: ApiRequests,
@@ -20,21 +21,7 @@ export class LoginService {
     const options = this.apiRequests.createRequestOptionsWithApplicationJsonHeaders();
     return this.http.post("/api/userauth/loginuser", encryptedLoginInfo, options)
       .map(this.apiRequests.parseResponse)
-      .retryWhen((error) => {
-        return error.scan((errorCount, err) => {
-          if (errorCount === 5) {
-            throw err;
-          }
-          switch (err.status) {
-            case 401:
-              throw err;
-            case 503:
-              return errorCount + 1;
-            default:
-              return errorCount + 1;
-          }
-        }, 0);
-      })
+      .retryWhen((error) => this.apiRequests.checkStatusCodeForRetry(this.codesToNotRetry, error))
       .catch(this.apiRequests.errorCatcher);
   }
 }
