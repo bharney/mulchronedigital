c19685e7a00a5d79f2e2f73e9f8b71f981ec11bd
Mike Mulchrone <mtmulch@gmail.com>
2018-04-12 23:56:52 -0500

    #73 adding 5 retrys on the activate user route.

--- client/app/shared/services/activate-user.service.ts
+++ client/app/shared/services/activate-user.service.ts
@@ -3,8 +3,6 @@ import { Http, RequestOptions, Response } from "@angular/http";
 import { ApiRequests } from "../http/ApiRequests";
 import { Observable } from "rxjs/Observable";
 import { ActivateUser } from "../models/user-authenication.model";
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/catch";
 import { AESEncryptionResult } from "../../../../shared/AESEncryptionResult";
 
 @Injectable()
@@ -18,8 +16,22 @@ export class ActivateUserService {
     public makeUserActive(encryptedUserObject: AESEncryptionResult) {
         const headers = this.apiRequests.createRequestOptionsWithApplicationJsonHeaders();
         return this.http.patch("/api/userauth/activateuser", encryptedUserObject, headers)
-        .map(this.apiRequests.parseResponse)
-        .catch(this.apiRequests.errorCatcher);
+            .map(this.apiRequests.parseResponse)
+            .retryWhen((error) => {
+                return error.scan((errorCount, err) => {
+                    if (errorCount === 5) {
+                        throw err;
+                    }
+                    switch (err.status) {
+                        case 409:
+                        case 401:
+                            throw err;
+                        case 503:
+                            return errorCount + 1;
+                    }
+                }, 0);
+            })
+            .catch(this.apiRequests.errorCatcher);
     }
 
 }
