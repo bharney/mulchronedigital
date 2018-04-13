076a9f5d26596d841a4175eef49b51ea6ccfaf54
Mike Mulchrone <mtmulch@gmail.com>
2018-04-13 01:16:28 -0500

    #73 Refactroing RegisterService

--- client/app/shared/services/user-authenication.service.ts
+++ client/app/shared/services/user-authenication.service.ts
@@ -11,6 +11,7 @@ import IService from "./interfaces/IService";
 @Injectable()
 export class LoginService implements IService {
   public codesToNotRetry: number[] = [401, 422];
+  
   constructor(
     private http: Http,
     private apiRequests: ApiRequests,
@@ -27,7 +28,8 @@ export class LoginService implements IService {
 }
 
 @Injectable()
-export class RegisterService {
+export class RegisterService implements IService {
+  public codesToNotRetry: number[] = [401, 422, 409];
 
   constructor(
     private http: Http,
@@ -39,23 +41,7 @@ export class RegisterService {
     const options = this.apiRequests.createRequestOptionsWithApplicationJsonHeaders();
     return this.http.post("/api/userauth/registeruser", encryptedNewUserObject, options)
       .map(this.apiRequests.parseResponse)
-      .retryWhen((error) => {
-        return error.scan((errorCount, err) => {
-          if (errorCount === 5) {
-            throw err;
-          }
-          switch (err.status) {
-            case 401:
-            case 422:
-            case 409:
-              throw err;
-            case 503:
-              return errorCount + 1;
-            default:
-              return errorCount + 1;
-          }
-        }, 0);
-      })
+      .retryWhen((error) => this.apiRequests.checkStatusCodeForRetry(this.codesToNotRetry, error))
       .catch(this.apiRequests.errorCatcher);
   }
 }
