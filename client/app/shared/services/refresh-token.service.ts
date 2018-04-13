35d73cb87c07cd84dec91485bae8393cf7acdb32
Mike Mulchrone <mtmulch@gmail.com>
2018-04-13 00:08:29 -0500

    #73 adding retryWhen into refresh-token service

--- client/app/shared/services/refresh-token.service.ts
+++ client/app/shared/services/refresh-token.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from "@angular/core";
 import { Http, RequestOptions, Response } from "@angular/http";
 import { ApiRequests } from "../http/ApiRequests";
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/catch";
 
 @Injectable()
 export class RefreshTokenService {
@@ -17,6 +15,21 @@ export class RefreshTokenService {
     const options = this.apiRequests.createAuthorizationHeader();
     return this.http.get("/api/userauth/refreshtoken", options)
       .map(this.apiRequests.parseResponse)
+      .retryWhen((error) => {
+        return error.scan((errorCount, err) => {
+          if (errorCount === 5) {
+            throw err;
+          }
+          switch (err.status) {
+            case 401:
+              throw err;
+            case 503:
+              return errorCount + 1;
+            default:
+              return errorCount + 1;
+          }
+        }, 0);
+      })
       .catch(this.apiRequests.errorCatcher);
   }
 }
